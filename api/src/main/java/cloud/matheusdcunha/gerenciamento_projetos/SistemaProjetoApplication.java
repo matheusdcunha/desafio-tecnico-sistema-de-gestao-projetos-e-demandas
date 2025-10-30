50d381430586c2cc8e99e16ce4177215e8717ce7
Matheus da Cunha <matheus.cunha.fonseca50@gmail.com>
2025-10-30 20:49:16 -0300

    build: estruturação inicial - closes #01

---
+++ api/src/main/java/cloud/matheusdcunha/gerenciamento_projetos/SistemaProjetoApplication.java
@@ -0,0 +1,13 @@
+package cloud.matheusdcunha.gerenciamento_projetos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaProjetoApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/cloud/matheusdcunha/gerenciamento_projetos/SistemaProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.matheusdcunha.gerenciamento_projetos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
